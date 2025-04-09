08990a7463d26b9b22df9181b0ed7706fc61f0df
Michał Pietrucha <mpietrucha50@gmail.com>
2025-04-09 13:05:01 +0200

    wip

--- src/interacts-with-protected.js
+++ src/interacts-with-protected.js
@@ -1,12 +1,28 @@
 import { createProtected } from '@/protected'
-import { not } from '@mpietrucha/is'
+import { isEmpty, notInstanceOf } from '@mpietrucha/is'
 
 export class InteractsWithProtected {
+    #protected
+
+    constructor(...properties) {
+        this.#protected = properties
+
+        createProtected(this.constructor, 'protected', () => {
+            throw new Error('Cannot call `protected` outside constructor')
+        })
+    }
+
+    protected() {
+        return this.#protected
+    }
+
     static protected(source, ...properties) {
-        if (not(source, this)) {
+        if (notInstanceOf(source, this)) {
             return
         }
 
-        return createProtected(source, ...properties)
+        isEmpty(properties) && properties.push(source.protected())
+
+        properties.flat().forEach(property => createProtected(source, property))
     }
 }
--- src/protected.js
+++ src/protected.js
@@ -1,7 +1,12 @@
+import { isFunction } from '@mpietrucha/is'
 import { set } from 'lodash-es'
 
-export const createProtected = (source, ...properties) => {
-    properties.flat().forEach(property => set(source, property))
+export const createProtected = (source, property, handler) => {
+    if (isFunction(handler)) {
+        return set(source, property, handler)
+    }
 
-    return source
+    return createProtected(source, property, () => {
+        throw new Error(`Property \`${property}\` is protected`)
+    })
 }
